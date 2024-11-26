2ad8dd4dafbbdcb510f016c470fa8bf1db5bf35c
IanNotermansPXL <ian.notermans@student.pxl.be>
2024-11-26 21:19:20 +0100

    Backend microservices configured
    - discoveryService
    - configService

--- backend-java/review-service/src/main/java/be/pxl/services/ReviewServiceApplication.java
+++ backend-java/review-service/src/main/java/be/pxl/services/ReviewServiceApplication.java
@@ -1,11 +1,15 @@
 package be.pxl.services;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * Hello world!
  *
  */
+@SpringBootApplication
+@EnableDiscoveryClient
 public class ReviewServiceApplication
 {
     public static void main( String[] args )
